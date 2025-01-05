3c7b51aa4c2c613e411b731b979c14a0b3d34d57
Akmal <32015620+KingMaldio@users.noreply.github.com>
2025-01-05 08:00:14 +0700

    Update index.jsx

--- src/components/Mempelai/index.jsx
+++ src/components/Mempelai/index.jsx
@@ -12,8 +12,8 @@ const Mempelai = () => {
   const { wanita, pria } = useDB((db) => db.wedding.mempelai);
   return (
     <Box>
-      <ProfilMempelai mempelai={wedding.mempelai.pria} />
-      <ProfilMempelai mempelai={wedding.mempelai.wanita} />
+      <ProfilMempelai mempelai={pria} />
+      <ProfilMempelai mempelai={wanita} />
     </Box>
   );
 };
